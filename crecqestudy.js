3d09f43757592403332cc0deb24c51531a669c45
kevinmattew <36357431+kevinmattew@users.noreply.github.com>
2026-05-21 09:19:49 +0800

    Add update and download URLs for the script

--- crecqestudy.js
+++ crecqestudy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         中国中铁安全生产监督管理系统挂课助手 v5.20
-// @updateURL
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/kevinmattew/CREC-QE-study/refs/heads/main/crecqestudy.js
+// @downloadURL  https://raw.githubusercontent.com/kevinmattew/CREC-QE-study/refs/heads/main/crecqestudy.js
 // @namespace    http://tampermonkey.net/
 // @version      5.20.0
 // @description  全自动刷课助手 - 增加详细调试信息
